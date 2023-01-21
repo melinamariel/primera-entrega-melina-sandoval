310c8d4692c23736ecd691cf3805b35844ff8ef8
melina <melina_sandoval_27@hotmail.com>
2023-01-21 19:29:53 -0300

    modificaciones

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let nombre 
+let apellido
+let celular
+let numero = 1
+
+
+nombre = prompt ("ingrese nombre")
+if (nombre == ""){
+    alert("no ingresaste ningun dato")
+    nombre = prompt ("ingrese nombre") 
+    
+}else{
+    alert("nombre ingresado:" + " " + nombre)
+}
+
+
+apellido = prompt ("ingrese apellido")
+if (nombre == ""){
+    alert("no ingresaste ningun dato")
+    apellido = prompt ("ingrese apellido") 
+    
+}else{
+    alert("apellido ingresado:" + " " + apellido)
+}
+
+
+
+celular = Number ( prompt ("ingrese numero de celular "))
+if (celular == ""){
+    alert("no ingresaste ningun dato")
+    celular = Number (prompt ("ingrese numero de celular")) 
+    
+}else{
+    alert("celular ingresado:" + " " + celular)
+}
+
+for (let numero = 1; numero <= 100; numero++){
+    alert(nombre + " " + apellido + " " + "tu numero es:" + " " + numero )
+    break
+}
+
+function texto (saludos) {
+    let suerte = "gracias por participar, suerte!"
+
+   alert( saludos + " " + nombre + " " + " " + suerte )
+}
+    texto ("hola")
+
+    console.log(nombre + " " + apellido + " " + "tu numero es:" + " " + numero )
+
+
+
+
+
+
+
+
